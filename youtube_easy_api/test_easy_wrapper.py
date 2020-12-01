8c4babe9c74702112374214d1a883d42c94138ef
Pedram Ataee <pedram.ataee@gmail.com>
2020-12-01 12:18:33 -0800

    Add one more test + improve authentication

--- youtube_easy_api/test_easy_wrapper.py
+++ youtube_easy_api/test_easy_wrapper.py
@@ -3,25 +3,35 @@ from youtube_easy_api.easy_wrapper import *
 PROJECT_PATH = os.path.abspath(os.path.join(os.path.abspath(__file__), os.pardir))
 CREDENTIALS_PATH = '../../../Secrets/YouTube'
 
+f = open(os.path.join(CREDENTIALS_PATH, 'api.txt'), "r")
+API_KEY = f.read()
+
 
 def test_get_metadata_01():
     easy_wrapper = YoutubeEasyWrapper()
-    easy_wrapper.initialize(credentials_path=CREDENTIALS_PATH)
+    easy_wrapper.initialize(api_key=API_KEY)
     metadata = easy_wrapper.get_metadata(video_id='f3lUEnMaiAU')
     print(metadata['comments'][0])
 
 
 def test_get_metadata_02():
     easy_wrapper = YoutubeEasyWrapper()
-    easy_wrapper.initialize(credentials_path=CREDENTIALS_PATH)
+    easy_wrapper.initialize(api_key=API_KEY)
     metadata = easy_wrapper.get_metadata(video_id='rdjnkb4ONWk')
     print(metadata['statistics']['likeCount'])
     assert metadata['title'] == 'The Pink Panther Show Episode 59 - Slink Pink'
 
 
+def test_get_metadata_03():
+    easy_wrapper = YoutubeEasyWrapper()
+    easy_wrapper.initialize(api_key=API_KEY)
+    metadata = easy_wrapper.get_metadata(video_id='BW4fYZHVhfk')
+    print(metadata['statistics']['commentCount'])
+
+
 def test_search_videos_01():
     easy_wrapper = YoutubeEasyWrapper()
-    easy_wrapper.initialize(credentials_path=CREDENTIALS_PATH)
+    easy_wrapper.initialize(api_key=API_KEY)
     results = easy_wrapper.search_videos(search_keyword='python', order='relevance')
     order_id = 1
     video_id = results[order_id]['video_id']
@@ -30,7 +40,7 @@ def test_search_videos_01():
 
 def test_search_videos_02():
     easy_wrapper = YoutubeEasyWrapper()
-    easy_wrapper.initialize(credentials_path=CREDENTIALS_PATH)
+    easy_wrapper.initialize(api_key=API_KEY)
     results = easy_wrapper.search_videos(search_keyword='python', order='relevance')
     order_id = 1
     print(results[order_id]['video_id'])
